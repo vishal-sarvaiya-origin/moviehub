22b56dbc41dd8175ce568ff470b3a1b5e8e363b4
Vishal <vishal.origin123@gmail.com>
2022-05-21 12:10:17 +0530

    make some changes in both test file on branch1

--- test.php
+++ test.php
@@ -1,1 +1,2 @@
 Make some changes in test.php on master branch by vishal on vs code
+need to make some changes in test.php on branch1 branch by vishal on vs code
--- test2.php
+++ test2.php
@@ -1,1 +1,2 @@
 Make some changes in test2.php on branch1 branch by vishal on vs code
+Add some more lines in test2.php on branch1 branch by vishal on vs code
