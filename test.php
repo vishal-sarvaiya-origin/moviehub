f3d39efd1147c9262ae44ceac9a6819298ffed06
Vishal <vishal.origin123@gmail.com>
2022-05-21 12:12:18 +0530

    make some changes in b file on master branch

--- test.php
+++ test.php
@@ -1,2 +1,3 @@
 Make some changes in test.php on master branch by vishal on vs code
+Make some changes in test.php on master branch by vishal on vs code
 and one line
--- test2.php
+++ test2.php
@@ -1,1 +1,2 @@
 Make some changes in test2.php on branch1 branch by vishal on vs code
+Make some changes in test2.php on master branch by vishal on vs code
